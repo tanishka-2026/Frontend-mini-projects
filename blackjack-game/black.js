769a5b9be4ca0eb3dfa38b517be633e5347c2e25
tanishka-2026 <tanishka.p2026@gmail.com>
2026-06-30 22:46:07 +0530

    blackjack condition update

---
+++ blackjack-game/black.js
@@ -0,0 +1,34 @@
+
+let firstcard=7
+let secondcard=4
+sum= firstcard+secondcard
+console.log(sum)
+
+if(sum <= 20)
+{
+    console.log("Do you want to draw a new card?")
+}
+else if (sum===21)
+{
+    console.log("Woohoo! You got Blackjack!")
+}
+else
+{
+    console.log("Sorry, you are out of the game! :(")
+}
+
+/*
+let age = 100
+if(age<100)
+{
+    console.log("not eligible")
+}
+if(age===100)
+{
+    console.log("here's your birthday card from the King!")
+}
+else
+{
+    console.log("Not eligible, you already have gotten one")
+}
+*/
