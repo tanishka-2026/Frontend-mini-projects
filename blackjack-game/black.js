0f5f85aaeec3a9e789901adb45efa143d729c0cd
tanishka-2026 <tanishka.p2026@gmail.com>
2026-07-04 21:14:28 +0530

    main game screen

--- blackjack-game/black.js
+++ blackjack-game/black.js
@@ -1,28 +1,30 @@
-
+let sum=0
 let firstcard=7
 let secondcard=4
 sum= firstcard+secondcard
-console.log(sum)
-
 let hasblackjack=false
 let isAlive= true
-
 let message = ""
-if(sum <= 20)
-{
-    message = "Do you want to draw a new card?"
+let messageEl=document.getElementById("message-el")
+let sumEl=document.getElementById("sum-el")
+function startGame() {
+    
+    if(sum <= 20)
+    {
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum===21)
+    {
+        message= "Woohoo! You got Blackjack!"
+        hasblackjack=true
+    }
+    else
+    {
+       message="Sorry, you are out of the game! :("
+       isAlive = false
+    }
+    console.log(message)
 }
-else if (sum===21)
-{
-    message= "Woohoo! You got Blackjack!"
-    hasblackjack=true
-}
-else
-{
-    message="Sorry, you are out of the game! :("
-    isAlive = false
-}
-console.log(message)
 
 /*
 let age = 100
