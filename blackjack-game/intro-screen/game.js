f587d13d9a3e988abf45f94145513823812f34c3
tanishka-2026 <tanishka.p2026@gmail.com>
2026-07-10 23:13:34 +0530

    sum of cards input

--- blackjack-game/intro-screen/game.js
+++ blackjack-game/intro-screen/game.js
@@ -9,6 +9,7 @@ let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el")
 
 function startGame() {   
+    sumEl.textContent=""+sum
     if(sum <= 20)
     {
         message = "Do you want to draw a new card?"
