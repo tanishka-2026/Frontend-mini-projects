efe19697c37cc83e9b22cf211a5f58c75fe12fd7
tanishka-2026 <tanishka.p2026@gmail.com>
2026-07-19 12:24:39 +0530

    sum of cards

--- blackjack-game/intro-screen/game.js
+++ blackjack-game/intro-screen/game.js
@@ -1,29 +1,58 @@
-let sum=0
-let firstcard=7
-let secondcard=4
-sum= firstcard+secondcard
-let hasblackjack=false
-let isAlive= true
-let message = ""
-let messageEl=document.querySelector("#message-el")
-let sumEl=document.querySelector("#sum-el")
-let cardsEl=document.querySelector("#cards-el")
-function startGame() { 
-    cardsEl.textcontent="Cards: "+firstcard+" "+secondcard  
-    sumEl.textContent=""+sum
-    if(sum <= 20)
-    {
-        message = "Do you want to draw a new card?"
+// Starting cards
+let firstCard = 7;
+let secondCard = 4;
+
+// Total
+let sum = firstCard + secondCard;
+
+// Store cards
+let cards = [firstCard, secondCard];
+
+// Game status
+let hasBlackJack = false;
+let isAlive = true;
+
+// Message
+let message = "";
+
+// HTML elements
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame() {
+
+    cardsEl.textContent =  cards.join(" ");
+    sumEl.textContent = sum;
+
+    if (sum <= 20) {
+
+        message = "Do you want to draw a new card?";
+
+    } else if (sum === 21) {
+
+        message = "Woohoo! You've got Blackjack!";
+        hasBlackJack = true;
+
+    } else {
+
+        message = "Sorry, you're out of the game!";
+        isAlive = false;
     }
-    else if (sum===21)
-    {
-        message= "Woohoo! You got Blackjack!"
-        hasblackjack=true
+
+    messageEl.textContent = message;
+}
+
+function newCard() {
+
+    if (isAlive && !hasBlackJack) {
+
+        let card = 8;
+
+        cards.push(card);
+
+        sum += card;
+
+        startGame();
     }
-    else
-    {
-       message="Sorry, you are out of the game! :("
-       isAlive = false
-    }
-    messageEl.textContent=message
 }
